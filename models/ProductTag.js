ed2e566bf0d7b79d330305818282cd0e6c8a3b58
Michael Bashford <mikebashford@gmail.com>
2022-02-06 14:15:34 -0600

    Added data fields for models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,34 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
+const { Product, Tag } = require(".");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: Product.id,
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: Tag.id,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
